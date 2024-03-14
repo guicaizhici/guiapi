eba2e32d631b43d78e30f1ee05958c4a86f1f390
guicaizhici <68894564+guicaizhici@users.noreply.github.com>
2024-03-14 14:36:45 +0800

    提交网关类，用户调用接口请求经过网关

---
+++ guiapi-gateway/src/test/java/com/cheng/apitoolgateway/ApitoolGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheng.apitoolgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitoolGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
